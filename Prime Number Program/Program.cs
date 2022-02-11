27449e739e4fe10d776674a7d4c4751610513714
indhumathi <Indhumathi.Ramesh@disys.com>
2022-02-11 06:52:04 -0800

    Add project files.

---
+++ Prime Number Program/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Primenumber
+{
+    public static void Main()
+    {
+        int number = 7, value = 0;
+        for (int valueTwo = 1; valueTwo <= number; valueTwo++)
+        {
+            if (number % valueTwo == 0)
+            {
+                value++;
+            }
+        }
+        if (value == 2)
+        {
+            Console.WriteLine("prime number:" + number);
+        }
+        else
+        {
+            Console.WriteLine("Not a Prime Number:" + number);
+        }
+    }
+}
+
